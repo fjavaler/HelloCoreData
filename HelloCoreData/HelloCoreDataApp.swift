8376c7b55e01113ee928e79aca3199630697b2af
Frederick Javalera <frederickjavalera@gmail.com>
2021-11-20 12:41:33 -0700

    Adding CoreData objects

--- HelloCoreData/HelloCoreDataApp.swift
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct HelloCoreDataApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView(storageProvider: StorageProvider())
     }
+  }
 }
